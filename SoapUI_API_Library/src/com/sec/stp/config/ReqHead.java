4190249e2ab6f3feccba66594c614abb8b4db82f
HanRim <hrlee@testenc.com>
2016-04-27 15:19:11 +0900

    Revert "Delete All"
    This reverts commit 3ca2e4d18d36bcf8ff675f7681b3abd485fd3b7d.

---
+++ SoapUI_API_Library/src/com/sec/stp/config/ReqHead.java
@@ -0,0 +1,48 @@
+package com.sec.stp.config;
+
+import java.util.ArrayList;
+
+public class ReqHead {
+	
+	public ArrayList <String> headerKeys = new ArrayList <String>();
+	public ArrayList <String> headerValues = new ArrayList <String>();
+	
+	
+	public void setHeader(String key , String value){
+		this.headerKeys.add(key);
+		this.headerValues.add(value);
+	}
+	public ArrayList <String> getHeaderKeys(){
+		try{
+			return headerKeys;
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	public ArrayList <String> getHeaderValues(){
+		try{
+			return headerValues;
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	
+	}
+	public String getRequestHeader(String keyName) throws Exception{
+		if(headerKeys.contains(keyName) == true){
+			int index = headerKeys.indexOf(keyName);
+			return headerValues.get(index);
+		}else{
+			return null;
+		}
+	}
+		
+	public ArrayList<String> getStatus() throws Exception{
+			if(headerKeys != null){
+				return headerKeys;
+			}else{
+				return headerValues;
+			}
+	}
+}
---
+++ SoapUI_API_Library/src/com/sec/stp/config/ResPayload.java
@@ -0,0 +1,13 @@
+package com.sec.stp.config;
+
+public class ResPayload {
+
+	public Object payload;
+	public Object getPayload(){
+		return payload;
+	}
+	public void setPayloadAsString(Object payload) {
+		this.payload = (Object) payload;
+	}
+	
+}
